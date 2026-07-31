41ee24a6a6d4fdb21aa859161f934b95b0757216
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-07-31 20:16:15 +0500

    feat : attributes & methods added

--- first.py
+++ first.py
@@ -1,9 +1,19 @@
 class Student:
+      college_name="Shalimar College"
+      name="mehran"
       #parameterized Constructor
-      def __init__(self,username):
-           print(self)
-           self.name=username   
-      
-s1=Student("Usman")
-print(s1)
+      def __init__(self,username,marks):
+           self.name=username
+           self.marks=marks
 
+      
+      def getAvgMarks(self):
+          sum=0
+          for i in self.marks:
+              sum+=i
+          avg=sum/300
+          return avg
+      
+s1=Student("Usman",[34,45,33])
+print(s1.getAvgMarks()) 
+s1.name="Aleem" # again you can overide the name
