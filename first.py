430ed40c3b8ed19b9a233c68a447129dddeebe74
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-08-03 17:58:30 +0500

    feat : add mult functionality + auto suggestion off

--- first.py
+++ first.py
@@ -1,12 +1,16 @@
 class Order:
-     def __init__(self,amount):
-          self.amount = amount
+       def __init__(self,num1):
+              self.num1=num1
+
+       def show(self):
+              print(self.num1)
+
+       def __mul__(self,numInfo):
+              num3=self.num1*numInfo.num1
+              return Order(num3)
 
 
-class OrderProcessor(Order):
-        def process(self):
-            print(f"Processing order of amount: {self.amount}")
-
-
-o1=OrderProcessor(100)
-o1.process()
+n1=Order(5)
+n2=Order(7)
+n3=n1*n2
+n3.show()
