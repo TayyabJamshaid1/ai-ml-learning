370bffac5ece466a6fbfda595a7ede13728164dd
tayyabjamshaid <tayyab.devblends@gmail.com>
2026-08-04 19:23:01 +0500

    feat : problems resolved

--- first.py
+++ first.py
@@ -1,3 +1,11 @@
-l=[9,8,2]
-squardData=[i*i for i in l]
-print(squardData)
+num=int(input("enter number : "))
+
+try:
+    with(open("tables.txt","a") as f1):
+
+        table=[f"{num} X {i} = {num*i}\n" for i in range(1,11)]
+        for i in table:
+             f1.write(i)
+
+except Exception as e:
+      print(e)
